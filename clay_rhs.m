535bc7b879ec8a6d740f9b4bd350b5fc093dcc33
Jake Bowhay <jb9.bowhay@gmail.com>
2023-03-20 12:26:52 +0000

    angle dependant drag

--- clay_rhs.m
+++ clay_rhs.m
@@ -1,10 +1,10 @@
-function dydt = clay_rhs(t, y, M, A, rho, Cd, Cl, theta)
+function dydt = clay_rhs(t, y, M, A, rho, Cd_0, Cd_alpha, Cl_0, Cl_alpha, theta)
 %[x,y,z,u,v,w] note v should always be zero then rotate afterwards
-alpha = atan2(y(6),y(4));
+alpha = abs(theta - atan2(y(6),y(4))); % angle of attack
 v = norm(y(4:end));
 
-D = 0.5 * rho * v^2 * Cd * A; % drag
-L = 0.5 * rho * v^2 * Cl * A; % lift
+D = 0.5 * rho * v^2 * (Cd_0 + alpha^2 * Cd_alpha) * A; % drag
+L = 0.5 * rho * v^2 * (Cl_0 + alpha * Cl_alpha) * A; % lift
 
 dydt = [y(4), y(5), y(6),...
     -(D/M)*cos(alpha-theta) - (L/M)*sin(alpha-theta), ...
--- simulator.m
+++ simulator.m
@@ -5,20 +5,23 @@ clay_y = 0;
 clay_launch_angle = 0.5;
 clay_launch_speed = 15.6;
 clay_mass = 0.105; % 105g weight
-clay_Cd = 0.2; % drag coefficient
+clay_Cd = 5.07; % drag coefficient
+clay_Cd_alpha = 0.077; % angle dependant drag coefficient
 clay_A = 0.0095; % clay area size
 rho = 1.184; % air density
-clay_CL = 0.1; % lift coefficient
+clay_C_l_0 = 1.17; % lift coefficient
+clay_C_l_alpha = 0.28; % angle dependant lift coefficient
+
 
 shot_x = 5;
 shot_y = 30;
 shot_theta = -pi/4;
 shot_phi = pi/3;
-shot_launch_speed = 30;
+shot_launch_speed = 3;
 
 options = odeset("Events", @ground_event);
 
-[t_clay, y_clay] = ode45(@(t, y) clay_rhs(t, y, clay_mass, clay_A, rho, clay_Cd, clay_CL, clay_launch_angle), [0, 100], ...
+[t_clay, y_clay] = ode45(@(t, y) clay_rhs(t, y, clay_mass, clay_A, rho, clay_Cd, clay_Cd_alpha, clay_C_l_0, clay_C_l_alpha, clay_launch_angle), [0, 100], ...
     [0, 0, 0, clay_launch_speed*cos(clay_launch_angle), 0, clay_launch_speed*sin(clay_launch_angle)]', options);
 
 [t_shot, y_shot] = ode45(@shot_rhs, [0, 100], [shot_x, shot_y, 0,...
