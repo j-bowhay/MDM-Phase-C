c84ea5322675f748202123dae99a65848621a32d
Max Trivedi <maxtrivedi@icloud.com>
2023-03-20 08:06:27 +0000

    Update clay_rhs.m

--- clay_rhs.m
+++ clay_rhs.m
@@ -1,7 +1,8 @@
-function dydt = clay_rhs(t, y, m, Cd, A, rho, theta, phi)
+function dydt = clay_rhs(t, y, m, Cd, Cl, A, rho, theta, phi)
 %[x,y,z,u,v,w]
 v = sqrt(y(4)^2+y(5)^2+y(6)^2);
 dragForce = 0.5 * rho * v^2 * Cd * A;
+liftForce = 0.5 * rho * v^2 * Cl * A;
 dydt = [y(4), y(5), y(6), -dragForce*cos(theta), -dragForce*sin(theta), -9.81*m-dragForce*cos(phi)]';
 end
 
