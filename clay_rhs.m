e328c9a863c6dc28ecb50b74487d6347aab8901e
Max Trivedi <maxtrivedi@icloud.com>
2023-03-20 07:37:46 +0000

    Add drag equation

--- clay_rhs.m
+++ clay_rhs.m
@@ -1,6 +1,7 @@
-function dydt = clay_rhs(t, y)
+function dydt = clay_rhs(t, y, m, Cd, A, rho, theta, phi)
 %[x,y,z,u,v,w]
-% at the momement mass is just hard coded as 1; change this
-dydt = [y(4), y(5), y(6), 0, 0, -9.81]';
+v = sqrt(y(4)^2+y(5)^2+y(6)^2);
+dragForce = 0.5 * rho * v^2 * Cd * A;
+dydt = [y(4), y(5), y(6), -dragForce*cos(theta), -dragForce*sin(theta), -9.81*m-dragForce*cos(phi)]';
 end
 
