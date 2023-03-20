5f8db5a8a06e4d78d91e4ff7c74a46e68c4db8d2
Max Trivedi <maxtrivedi@icloud.com>
2023-03-20 08:41:50 +0000

    Added lift force and updated angles

--- clay_rhs.m
+++ clay_rhs.m
@@ -1,8 +1,12 @@
-function dydt = clay_rhs(t, y, m, Cd, Cl, A, rho, theta, phi)
+function dydt = clay_rhs(t, y, m, A, rho, Cd, Cl)
 %[x,y,z,u,v,w]
+alpha = atan(y(5)/ y(4));
+beta = atan(y(6)/ sqrt(y(4)^2 + y(5)^2));
 v = sqrt(y(4)^2+y(5)^2+y(6)^2);
+
 dragForce = 0.5 * rho * v^2 * Cd * A;
 liftForce = 0.5 * rho * v^2 * Cl * A;
-dydt = [y(4), y(5), y(6), -dragForce*cos(theta), -dragForce*sin(theta), -9.81*m-dragForce*cos(phi)]';
+
+dydt = [y(4), y(5), y(6), -dragForce*cos(alpha), -dragForce*sin(alpha), -9.81*m+liftForce*sin(beta)]';
 end
 
